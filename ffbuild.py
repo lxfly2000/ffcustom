00b988008d1035f99753a9a38f9d03e03e3a58bc
lxfly2000 <gaoboyuan8@qq.com>
2019-05-29 09:55:54 +0800

    Update.

--- ffbuild.py
+++ ffbuild.py
@@ -1,7 +1,7 @@
 # Python 3.7
 # Encoding: UTF-8
 
-JDK_PATH="C:/Program Files/Java/jdk1.8.0_202"
+JDK_PATH="D:/jdk-12.0.1"
 
 if __name__!="__main__":
     print("请通过命令行运行这个脚本。")
@@ -26,6 +26,7 @@ assets_path=project_dir+"/assets"
 # ffconfig 字体名 {字体替换组}
 if len(sys.argv)<=1:
     print("命令行：ffbuild.py <字体名> <字体文件1> <替换文件1> [<字体文件2> <替换文件2>…]")
+    print("　　　　ffbuild.py <字体名> <常规字体文件> [粗体字体文件]")
     print("字体文件：指要显示的字体的所需文件。")
     print("替换文件：指系统中原来存在的文件，比如DroidSans.ttf，DroidSans-Bold.ttf，DroidSansFallback.ttf等，\n　　　　　注意必须含有DroidSans.ttf一项。")
     print("\n使用方法：\n 1.先用本程序生成字体包，\n 2.将生成的APK安装至手机，\n 3.在系统设置里选择生成的字体。")
@@ -91,11 +92,23 @@ print("配置 \"%s\"…"%(sys.argv[1]))
 clear_dir(assets_path+"/fonts")
 clear_dir(assets_path+"/xml")
 xml_name=""
+if len(sys.argv)==3:
+    sys.argv.append("DroidSans.ttf")
+    sys.argv.append(sys.argv[2])
+    sys.argv.append("DroidSansFallback.ttf")
+elif len(sys.argv)==4 and sys.argv[3].lower()[:9]!="droidsans":
+    sys.argv.append(sys.argv[3])
+    sys.argv[3]="DroidSans.ttf"
+    sys.argv.append("DroidSans-Bold.ttf")
+    sys.argv.append(sys.argv[2])
+    sys.argv.append("DroidSansFallback.ttf")
 for i in range(2,len(sys.argv),2):
     if i+1<len(sys.argv):
         font_xml.addFont(sys.argv[i],sys.argv[i+1])
         if sys.argv[i+1].lower()=="droidsans.ttf":
             xml_name=sys.argv[i][:sys.argv[i].rfind(".")]
+            xml_name=xml_name.replace("\\","/")
+            xml_name=xml_name[xml_name.rfind("/")+1:]
 
 if xml_name=="":
     print("未指定 DroidSans.ttf 的替换字体。")
@@ -122,13 +135,13 @@ def save_string_app_name(app_name):
     print("APP_NAME 设置为 \""+app_name+"\"")
 
 def sign_apk(apk_path,signed_apk_path):
-    jarsigner="\""+JDK_PATH+"/bin/jarsigner.exe\""
+    jarsigner="\""+JDK_PATH+"/bin/jarsigner\""
     return os.system("echo ffkeys|"+jarsigner+" -keystore ffks.jks -storepass ffcustom -signedjar \""+signed_apk_path+"\" \""+apk_path+"\" cert")
 
 save_string_app_name(sys.argv[1])
 out_path=sys.argv[1]+".apk"
 sign_path=sys.argv[1]+"_signed.apk"
-if os.system("java -jar apktool.jar b -o\""+out_path+"\" "+project_dir):
+if os.system(JDK_PATH+"/bin/java -jar apktool.jar b -o\""+out_path+"\" "+project_dir):
     print("执行 apktool 出错，请检查是否已正确安装 Java 以及命令行是否正确。")
 else:
     if sign_apk(out_path,sign_path):
